b7f6b2ad5ec80331f23a39fde338820bcb7c8033
Teagan Durtschi <lakewingrasoftware@gmail.com>
2025-05-27 12:38:31 -0500

    Update dockerfile. Update test helpers and jasmine config

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM node:22.14.0-bookworm@sha256:e5ddf893cc6aeab0e5126e4edae35aa43893e2836d1d246140167ccc2616f5d7
-COPY package.json package-lock.json ./
+# Note: sometimes the apt-get fails to due caching. To fix, run:
+# docker buildx prune -f
 RUN apt-get clean
 RUN apt-get update
 RUN npx -y playwright@1.52.0 install --with-deps
+COPY package.json package-lock.json ./
 RUN npm install
 COPY . .
 CMD ["sh"]
--- test/helpers/setup.cjs
+++ test/helpers/setup.cjs
@@ -1,18 +1,14 @@
-// test/helpers/setup.cjs
 const { JSDOM } = require('jsdom');
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-// Create a simulated DOM environment
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = dom;
-
-// Assign to global to mimic browser globals
+// Set up global browser-like objects for tests
 global.window = window;
 global.document = window.document;
-global.navigator = { userAgent: 'node' };
+global.navigator = { userAgent: 'node.js' };
 
 require("../../libs/riot.js");
 global.riot = window.riot;
 
-// If your tests use other browser APIs, add polyfills here (e.g., for requestAnimationFrame)
-window.requestAnimationFrame = (callback) => setTimeout(callback, 0);
-window.cancelAnimationFrame = (id) => clearTimeout(id);
+// If jQuery or other libs need additional globals, add them here
+// (e.g., global.$ = require('jquery'); if needed, but imports handle it)
