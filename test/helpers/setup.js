f64a58db00deb34964db151a102172db9d085f47
Teagan Durtschi <lakewingrasoftware@gmail.com>
2025-05-27 10:29:02 -0500

    add model result

---
+++ test/helpers/setup.js
@@ -0,0 +1,15 @@
+// test/helpers/setup.js
+const { JSDOM } = require('jsdom');
+
+// Create a simulated DOM environment
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = dom;
+
+// Assign to global to mimic browser globals
+global.window = window;
+global.document = window.document;
+global.navigator = { userAgent: 'node' };
+
+// If your tests use other browser APIs, add polyfills here (e.g., for requestAnimationFrame)
+window.requestAnimationFrame = (callback) => setTimeout(callback, 0);
+window.cancelAnimationFrame = (id) => clearTimeout(id);
